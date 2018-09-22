b3713cb4879ea561f83876bf618db4514ad490a5
Rohan Tikmany <tikmanayrohan97@gmail.com>
2018-09-22 13:11:08 -0500

    Add mnist_data reader and README

---
+++ ADSA_BayesNaiveBayes_092318/README.md
@@ -0,0 +1,27 @@
+topics that we will be covering
+- Basic probability
+- Bayes Theorem
+- Binary Hypothesis Testing
+- Bayes Classifier
+- Naive Bayes Classifier
+
+
+## Steps to download and format data
+### Step 1
+Download data from this [link](https://pjreddie.com/projects/mnist-in-csv/)
+### Step 2
+Run the following command
+```
+python3 adsa_mnist_reader.py
+``` 
+This will create two files, test_data.json and train_data.json
+The data format for both test_data and train_data is:
+{
+'ClassName':[[pix00,pix01,pix02.....pix2727],[]......]
+.
+.
+.
+
+}
+where pixXY is the pixel at the Xth row and Yth columns
+and it's value is 0 for background and 1 for foreground
---
+++ ADSA_BayesNaiveBayes_092318/adsa_mnist_reader.py
@@ -0,0 +1,48 @@
+import csv
+from collections import defaultdict
+import json
+class reader:
+    def __init__(self,lim=100):
+        self.train,self.test = self._get_data(lim)
+        self._print_list(self.train['0'])
+        with open("train_data.json",'w') as fout:
+            json.dump(self.train,fout)
+        with open("test_data.json",'w') as fout:
+            json.dump(self.test,fout)
+    @staticmethod
+    def _print_list(print_list):
+        for digit in print_list:
+            for row in range(28):
+                for column in range(28):
+                    print(digit[28*row+column],end="")
+                print()
+            print()
+
+    @staticmethod
+    def _get_data(lim=100):
+        train = defaultdict(list)
+        test = defaultdict(list)
+        with open('mnist_train.csv','r') as fin:
+            mnist_reader = csv.reader(fin)
+            for digit in mnist_reader:
+                cur_elem=[]
+                for element in range(1,len(digit)):
+                    if int(digit[element]) > lim:
+                        cur_elem.append(1)
+                    else:
+                        cur_elem.append(0)
+                train[digit[0]].append(cur_elem)
+
+        with open('mnist_test.csv','r') as fin:
+            mnist_reader = csv.reader(fin)
+            for digit in mnist_reader:
+                cur_elem=[]
+                for element in range(1,len(digit)):
+                    if int(digit[element]) > lim:
+                        cur_elem.append(1)
+                    else:
+                        cur_elem.append(0)
+                test[digit[0]].append(cur_elem)
+        return train, test
+
+reader()
