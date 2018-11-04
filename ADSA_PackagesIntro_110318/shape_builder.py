61eb606b3932206ea779d799c4aef13b9ad26a23
Bailey Tincher <btincher99@gmail.com>
2018-11-04 01:49:28 -0500

    Add main fi to packages intro

--- ADSA_PackagesIntro_110318/shape_builder.py
+++ ADSA_PackagesIntro_110318/shape_builder.py
@@ -1,11 +1,3 @@
-from Shapes.Circle import Circle
-from Shapes.Square import Square
 
 if __name__ == '__main__':
-    c1 = Circle(10)
-
-    print(c1)
-
-    sq1 = Square(10)
-
-    print(sq1)
+    # TODO: Construct Square and Circle objects
